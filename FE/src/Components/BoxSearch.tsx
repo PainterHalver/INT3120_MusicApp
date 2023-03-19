5402a6719da47d9b35fb3f746fecf178481b3790
TrinhChung <chungtrinh2k2@gmail.com>
2023-03-20 00:12:48 +0700

    fix eslint

--- FE/src/Components/BoxSearch.tsx
+++ FE/src/Components/BoxSearch.tsx
@@ -1,92 +1,82 @@
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {View, TextInput, Text, StyleSheet} from 'react-native';
 const BoxSearch = () => {
-    return (
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+        paddingVertical: 10,
+      }}>
       <View
         style={{
-          flexDirection: 'row',
+          borderWidth: 1,
+          borderRadius: 50,
+          justifyContent: 'center',
           alignItems: 'center',
-          gap: 10,
-          paddingVertical: 10,
+          height: 30,
+          width: 30,
         }}>
-        <View
-          style={{
-            borderWidth: 1,
-            borderRadius: 50,
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: 30,
-            width: 30,
-          }}>
-          <Icon
-            name="user"
-            size={20}
-            color="black"
-            style={{fontWeight: '200'}}
-          />
-        </View>
-        <View
-          style={{
-            paddingHorizontal: 5,
-            borderWidth: 1,
-            borderColor: 'black',
-            borderRadius: 20,
-            flex: 1,
-            alignItems: 'center',
-            flexDirection: 'row',
-          }}>
-          <Text style={{paddingRight: 10}}>
-            <Icon
-              name="search"
-              size={20}
-              color="black"
-              style={{fontWeight: '200'}}
-            />
-          </Text>
-          <TextInput
-            style={styles.inputSearch}
-            placeholder="Tìm kiếm bài hát, nghệ sĩ"
-          />
-          <Text style={{paddingRight: 10}}>
-            <Icon
-              name="microphone"
-              size={20}
-              color="blue"
-              style={{fontWeight: '200'}}
-            />
-          </Text>
-        </View>
-        <View
-          style={{
-            borderRadius: 50,
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: 30,
-            width: 30,
-          }}>
-          <Icon
-            name="bell"
-            size={20}
-            color="black"
-            style={{fontWeight: '200'}}
-          />
-        </View>
+        <Icon name="user" size={20} color="black" style={{fontWeight: '200'}} />
       </View>
-    );
-  };
-export default BoxSearch
+      <View
+        style={{
+          paddingHorizontal: 5,
+          borderWidth: 1,
+          borderColor: 'black',
+          borderRadius: 20,
+          flex: 1,
+          alignItems: 'center',
+          flexDirection: 'row',
+        }}>
+        <Text style={{paddingRight: 10}}>
+          <Icon
+            name="search"
+            size={20}
+            color="black"
+            style={{fontWeight: '200'}}
+          />
+        </Text>
+        <TextInput
+          style={styles.inputSearch}
+          placeholder="Tìm kiếm bài hát, nghệ sĩ"
+        />
+        <Text style={{paddingRight: 10}}>
+          <Icon
+            name="microphone"
+            size={20}
+            color="blue"
+            style={{fontWeight: '200'}}
+          />
+        </Text>
+      </View>
+      <View
+        style={{
+          borderRadius: 50,
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: 30,
+          width: 30,
+        }}>
+        <Icon name="bell" size={20} color="black" style={{fontWeight: '200'}} />
+      </View>
+    </View>
+  );
+};
+export default BoxSearch;
 
-  const styles = StyleSheet.create({
-    inputSearch: {
-      flex: 1,
-      lineHeight: 35,
-      textAlignVertical: 'center',
-      padding: 0,
-    },
-    chipButton: {
-      borderRadius: 20,
-      borderWidth: 1,
-      paddingHorizontal: 5,
-      paddingVertical: 3,
-    },
-  });
+const styles = StyleSheet.create({
+  inputSearch: {
+    flex: 1,
+    lineHeight: 35,
+    textAlignVertical: 'center',
+    padding: 0,
+  },
+  chipButton: {
+    borderRadius: 20,
+    borderWidth: 1,
+    paddingHorizontal: 5,
+    paddingVertical: 3,
+  },
+});
--- FE/src/Components/ItemAlbum.tsx
+++ FE/src/Components/ItemAlbum.tsx
@@ -1,26 +1,25 @@
 import {View, Text, Image} from 'react-native';
 type Props = {
-    description: string;
-    image: string;
-    size:number;
+  description: string;
+  image: string;
+  size: number;
 };
-const ItemSong = ({description,image, size}:Props) => {
-    return (
-      <View style={{flexDirection: 'column', width: size, marginHorizontal:5}}>
-        <Image
-        source={{uri:image}}
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: size,
-            width: size,
-            borderRadius: 10,
-          }}
-        />
-        <Text style={{overflow:'hidden'}}>{description}</Text>
-      </View>
-    );
-  };
-
+const ItemSong = ({description, image, size}: Props) => {
+  return (
+    <View style={{flexDirection: 'column', width: size, marginHorizontal: 5}}>
+      <Image
+        source={{uri: image}}
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: size,
+          width: size,
+          borderRadius: 10,
+        }}
+      />
+      <Text style={{overflow: 'hidden'}}>{description}</Text>
+    </View>
+  );
+};
 
 export default ItemSong;
